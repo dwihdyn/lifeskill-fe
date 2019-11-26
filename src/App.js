91cf57974333185528b823146e84c71c6751eb0c
Dwi <jamaltan31@gmail.com>
2019-11-27 01:46:59 +0800

    API connected to backend for login/signup. proper redirect flow

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 // import axios from 'axios';
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 
-import LoginSignupPage from './Pages/LoginSignupPage'
-import StudentProfile from './Pages/StudentProfile'
+import LoginSignupPage from "./Pages/LoginSignupPage";
+import StudentProfile from "./Pages/StudentProfile";
 
 class App extends React.Component {
-	render() {
-		return (
-			<>
-				<Route
-					exact
-					path="/login"
-					component={() => {
-						return <LoginSignupPage />;
-					}}
-				/>
-				<Route
-					exact
-					path="/profile"
-					component={() => {
-						return <StudentProfile />;
-					}}
-				/>
-			</>
-		)
-	}
+  render() {
+    return (
+      <>
+        <Route
+          exact
+          path="/"
+          component={() => {
+            return <LoginSignupPage />;
+          }}
+        />
+        <Route
+          exact
+          path="/profile"
+          component={() => {
+            return <StudentProfile />;
+          }}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/Containers/LoginForm.js
+++ src/Containers/LoginForm.js
@@ -1,34 +1,97 @@
-import React from 'react'
-import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import React from "react";
+import { Container, Form, FormGroup, Label, Input } from "reactstrap";
+import { Redirect } from "react-router-dom";
+
+import axios from "axios";
 
 class LoginForm extends React.Component {
-    render() {
-        return (
-            <Container className="App">
-                <Form className="form">
-                    <FormGroup>
-                        <Label for="id_number">ID Number</Label>
-                        <Input
-                            type="text"
-                            name="id_number"
-                            id="id_number"
-                            placeholder="Your ID Number"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="password">Password</Label>
-                        <Input
-                            type="password"
-                            name="password"
-                            id="password"
-                            placeholder="********"
-                        />
-                    </FormGroup>
-                    <Button>Submit</Button>
-                </Form>
-            </Container>
-        )
+  state = {
+    id_number: ``,
+    password: ``,
+    redirect: false
+  };
+
+  handleIdNumber = e => {
+    console.log(e.target.value);
+    this.setState({
+      id_number: e.target.value
+    });
+  };
+
+  handlePassword = e => {
+    console.log(e.target.value);
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  handleLoginButton = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/v1/students/login", {
+        id_number: this.state.id_number,
+        password: this.state.password
+      })
+      .then(res => {
+        console.log(res);
+        if (res.data) {
+          this.setState({
+            redirect: true
+          });
+        } else {
+          alert("User does not exist, check the id or password");
+          this.setState({
+            id_number: ``,
+            password: ``
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    let { id_number, password, redirect } = this.state;
+
+    // page redirect must be under render, since we are rendering to new page
+    if (redirect) {
+      return <Redirect to="/profile" />;
     }
+
+    return (
+      <Container className="App">
+        <Form className="form">
+          <FormGroup>
+            <Label for="id_number">ID Number</Label>
+            <Input
+              type="text"
+              id="id_number"
+              placeholder="Your ID Number"
+              value={id_number}
+              onChange={this.handleIdNumber}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="********"
+              value={password}
+              onChange={this.handlePassword}
+            />
+          </FormGroup>
+          <input
+            type="submit"
+            placeholder="Submit"
+            onClick={this.handleLoginButton}
+          />
+        </Form>
+      </Container>
+    );
+  }
 }
 
-export default LoginForm
+export default LoginForm;

a99e66499666e9e46ab916ba93ef19b017e65210
Jules <julianawuan@gmail.com>
2019-12-01 17:39:52 +0800

    complete dashboard template

--- src/Pages/StudentProfile.js
+++ src/Pages/StudentProfile.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { Container, Col, Row, Button, Image } from "react-bootstrap";
+import {
+  Container,
+  Col,
+  Row,
+  Button,
+  Image,
+  Card,
+  ListGroup,
+  ListGroupItem,
+  Nav
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "../styles/StudentProfile.css";
+import profileImg from "../assets/profile_jw_small.png";
 
 import PointsWeekly from "../Containers/PointsWeekly";
 import PointsYearly from "../Containers/PointsYearly";
@@ -29,7 +41,27 @@ class StudentProfile extends React.Component {
           {/* <h1 style={{ paddingTop: 100, paddingBottom: 30 }}>My Dashboard</h1> */}
           <Row className="Dashboard-row">
             <Col className="Dashboard-sidebar" lg={2} sm={12}>
-              <Image src="../assets/profile_jw_small.png" roundedCircle />
+              <Image
+                className="Dashboard-sidebar-img"
+                src={profileImg}
+                roundedCircle
+                alt="profile pic"
+              />
+              <div className="Dashboard-sidebar-info">
+                <p>
+                  <strong>(Name)</strong>
+                </p>
+                <p>Year 9</p>
+              </div>
+              {/* Nav to display Points or Progress component */}
+              <Nav defaultActiveKey="/home" className="flex-column">
+                <Nav.Link eventKey="link-1" style={{ color: "#fa8d19" }}>
+                  My Points
+                </Nav.Link>
+                <Nav.Link eventKey="link-2" style={{ color: "#fa8d19" }}>
+                  My Progress
+                </Nav.Link>
+              </Nav>
             </Col>
             <Col className="Dashboard-charts" lg={10} sm={12}>
               <div className="Dashboard-charts-header">
@@ -44,13 +76,34 @@ class StudentProfile extends React.Component {
               <Row>
                 <Col className="Dashboard-points">
                   {" "}
-                  <h4>My Points</h4>
-                  <Button name="weekly" onClick={e => this.toggleView(e)}>
-                    Weekly
-                  </Button>
-                  <Button name="yearly" onClick={e => this.toggleView(e)}>
-                    Yearly
-                  </Button>
+                  <div className="Wrapper">
+                    <div className="Wrapper-header">
+                      {" "}
+                      <h4 className="Dashboard-points-header">My Points</h4>
+
+                      <p>Monday, 2/12/2019 - Sunday, 8/12/2019</p>
+                    </div>
+
+                    {/* Nav to toggle between weekly and yearly */}
+                    <div className="Wrapper-btn">
+                    <Button
+                        className="Dashboard-points-button"
+                        variant="warning"
+                        name="weekly"
+                        onClick={e => this.toggleView(e)}
+                      >
+                        Weekly
+                      </Button>
+                      <Button
+                        className="Dashboard-points-button"
+                        variant="warning"
+                        name="yearly"
+                        onClick={e => this.toggleView(e)}
+                      >
+                        Year-To-Date
+                      </Button>
+                    </div>
+                  </div>
                   {display_points}
                 </Col>
               </Row>
